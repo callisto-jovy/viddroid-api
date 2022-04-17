279ea747618e16e9c803eef4e8f01d20a706225f
yugata <romanportius@gmail.com>
2022-04-17 16:03:13 +0200

    Heroku port

--- routes/index.ts
+++ routes/index.ts
@@ -17,7 +17,7 @@ app.get("/", (req, res) =>
     }));
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Express API running on http://localhost:${PORT}`);
